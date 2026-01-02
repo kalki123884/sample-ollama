2c201620297b90f11f9a5dafc55f5918b41131db
kalki123884 <sivasankar.inkollu@yahoo.com>
2026-01-02 17:57:03 -0500

    upgraded qwen model

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if systemctl is-active k3s; then
     sudo /usr/local/bin/k3s-uninstall.sh
 fi
 docker-compose up -d
-docker exec -it ollama ollama pull qwen2.5:0.5b
+docker exec -it ollama ollama pull qwen2.5:1.5b
 echo "The script execition completed"
