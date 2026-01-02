d861818fa729ff07937510a2d3a7a379a96612b0
kalki123884 <sivasankar.inkollu@yahoo.com>
2026-01-02 10:42:27 -0500

    Added k3s uninstall

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+if systemctl is-active k3s; then
+    sudo /usr/local/bin/k3s-uninstall.sh
 docker-compose up -d
 docker exec -it ollama ollama pull qwen2.5:0.5b
 echo "The script execition completed"
