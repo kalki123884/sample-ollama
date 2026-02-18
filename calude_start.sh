a15809d5d528b80cc1c144dd70c6f4aa1f0ebe27
kalki123884 <sivasankar.inkollu@yahoo.com>
2026-02-17 22:53:50 -0500

    Create calude_start.sh

---
+++ calude_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xe
+if systemctl is-active k3s; then
+    sudo /usr/local/bin/k3s-uninstall.sh
+fi
+#docker-compose up -d
+curl -fsSL https://claude.ai/install.sh | bash -s stable
+curl -fsSL https://ollama.com/install.sh | sh
+#docker exec -it ollama ollama pull qwen2.5:1.5b
+ollama pull qwen2.5:1.5b
+echo "The script execition completed"
